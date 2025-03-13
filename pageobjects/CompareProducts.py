3a80e57d42c4b9b8da5bbe7c8778c39d32e611d2
michael crossley <michael.crossley8619@gmail.com>
2025-03-13 07:55:43 -0400

    adding code to check that items added are displayed in the compare list

--- pageobjects/CompareProducts.py
+++ pageobjects/CompareProducts.py
@@ -5,6 +5,8 @@ from utilities.CustomLog import LogGenerator
 class CompareProducts:
     # Define locators for webpage elements
     comp_head = "//span[@class='base' and @data-ui-id='page-title-wrapper' and text()='Compare Products']"
+    comp_gwen = "//a[normalize-space()='Gwen Drawstring Bike Short']"
+    comp_echo = "//a[normalize-space()='Echo Fit Compression Short']"
 
     def __init__(self, driver):
         # Initialize the driver and logger
@@ -16,6 +18,28 @@ class CompareProducts:
         self.logger.debug(f"Capturing the Compare Products header text")
         try:
             return wait_for_element(self.driver, self.comp_head).text
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return False
+
+    def compare_gwen_short_displayed(self):
+        # Log the action and check if the item is displayed
+        self.logger.debug(f"Checking if the item is displayed")
+        try:
+            element_displayed = wait_for_element(self.driver, self.comp_gwen).is_displayed()
+            print("Element displayed...")
+            return element_displayed
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return False
+
+    def compare_echo_short_displayed(self):
+        # Log the action and check if the item is displayed
+        self.logger.debug(f"Checking if the item is displayed")
+        try:
+            element_displayed = wait_for_element(self.driver, self.comp_echo).is_displayed()
+            print("Element displayed...")
+            return element_displayed
         except Exception as e:
             print(f"An error occurred: {e}")
             return False
