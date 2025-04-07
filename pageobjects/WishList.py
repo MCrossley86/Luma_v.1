0faece9e4fa3604438c8a4a222133fcfa4cbb1a7
MCrossley86 <michael.crossley8619@gmail.com>
2025-04-07 06:57:21 -0400

    PageObject Updates

--- pageobjects/WishList.py
+++ pageobjects/WishList.py
@@ -1,11 +1,14 @@
 # Import the necessary modules
 from utilities.CustomLog import LogGenerator
 from utilities.WaitForElements import wait_for_element
+from selenium.webdriver import ActionChains
 
 class MyWishList:
     # Define locators for webpage elements
     wish_list_head = "//span[@class='base' and @data-ui-id='page-title-wrapper' and text()='My Wish List']"
     wish_list_item = "//a[contains(text(),'Fiona Fitness Short')]"
+    fiona_Short_Hover = "//div[@data-container='product-grid']//img[@alt='Fiona Fitness Short']"
+    remove_fiona = "//a[@title='Remove Item']"
 
     def __init__(self, driver):
         # Initialize the driver and logger
@@ -31,3 +34,11 @@ class MyWishList:
         except Exception as e:
             print(f"An error occurred: {e}")
             return False
+
+    def remove_fiona_shorts(self):
+        # Log the action and click on "add to compare" icon
+        self.logger.debug(f"Clicking on add to compare icon")
+        act = ActionChains(self.driver)
+        hover_element = wait_for_element(self.driver, self.fiona_Short_Hover)
+        comp_element = wait_for_element(self.driver, self.remove_fiona)
+        act.move_to_element(hover_element).move_to_element(comp_element).click().perform()
