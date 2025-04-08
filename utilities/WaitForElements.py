11070abe4ca3aa19a26bbb4898ed5b28c164390a
MCrossley86 <michael.crossley8619@gmail.com>
2025-04-08 07:45:58 -0400

    WaitForElements updates - code to wait for elements to be clickable

--- utilities/WaitForElements.py
+++ utilities/WaitForElements.py
@@ -5,6 +5,13 @@ from selenium.webdriver.support import expected_conditions as ec
 from utilities.CustomLog import LogGenerator
 
 def wait_for_element(driver, locator, timeout=10):
+    # Log the action and wait for the element to be visible
     logger = LogGenerator.get_logger()
     logger.debug(f"Waiting for the element with locator: {locator}")
     return WebDriverWait(driver, timeout).until(ec.presence_of_element_located((By.XPATH, locator)))
+
+def element_clickable(driver, locator, timeout=10):
+    # Log the action and wait for the element to be clickable
+    logger = LogGenerator.get_logger()
+    logger.debug(f"Waiting for the element with locator: {locator}")
+    return WebDriverWait(driver, timeout).until(ec.element_to_be_clickable((By.XPATH, locator)))
