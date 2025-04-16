71c2890949c709adb0b0535a9ac8f9f53c183aca
MCrossley86 <michael.crossley8619@gmail.com>
2025-04-16 06:53:11 -0400

    Test_010_SearchField creation

---
+++ testcases/Test_010_SearchField.py
@@ -0,0 +1,50 @@
+# Import the necessary modules
+from pageobjects.HomePage import MainPage
+from pageobjects.SearchResults import SearchResults
+from utilities.ReadProperties import ReadConfig
+from utilities.CustomLog import LogGenerator
+import pytest
+import os
+
+class TestSearchField:
+    # Get the URL from the config file and initialize the logger
+    baseURL = ReadConfig.get_application_url()
+    logger = LogGenerator.get_logger()
+
+    @pytest.mark.sanity
+    def test_search_field(self, setup):
+        # Log the action, define and create a path to save screenshots
+        self.logger.info("*** Test_010_Search_Field Started ***")
+        self.driver = setup
+        screenshots_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'screenshots')
+        if not os.path.exists(screenshots_path):
+            os.makedirs(screenshots_path)
+
+        try:
+            # Log the action and navigate to the account registration page
+            self.logger.info("*** Navigate to the main page ***")
+            self.driver.get(self.baseURL)
+            self.driver.maximize_window()
+
+            # Log the action and search for a specific item
+            self.logger.info("*** Clicking in the search field and searching ***")
+            self.mp = MainPage(self.driver)
+            self.mp.click_search_fld()
+            self.mp.set_search_item("Argus")
+            self.mp.select_search_item()
+
+            # Log the action and check for the header title
+            self.logger.info("*** Checking for header title ***")
+            self.sr = SearchResults(self.driver)
+            self.conf_title = self.sr.capture_header_txt()
+            assert self.conf_title == "Search results for: 'argus'"
+            self.driver.quit()
+            self.logger.info("*** Test_010_Search_Field Passed ***")
+
+        except Exception as e:
+            # Log the action and capture the screenshot of any failure
+            self.logger.info("*** Test_010_Search_Field Failed ***")
+            screenshot_filename = os.path.join(screenshots_path, "test_search_field.png")
+            self.driver.save_screenshot(screenshot_filename)
+            raise e
+        self.logger.info("*** Test_010_Search_Field Complete ***")
