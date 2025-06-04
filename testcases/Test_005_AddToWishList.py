4da28b0bbe11dd11060a1254052c232786db5a4e
MCrossley86 <michael.crossley8619@gmail.com>
2025-06-03 20:04:16 -0400

    Code updates

--- testcases/Test_005_AddToWishList.py
+++ testcases/Test_005_AddToWishList.py
@@ -18,13 +18,19 @@ class TestAddToCompare:
     @pytest.mark.sanity
     def test_add_to_comp(self, setup):
         # Log the action, define and create a path to save screenshots
-        self.logger.info("*** Test_007_AddToWishList started ***")
+        self.logger.info("*** Test_005_AddToWishList started ***")
         self.driver = setup
         screenshots_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'screenshots')
         if not os.path.exists(screenshots_path):
             os.makedirs(screenshots_path)
 
         try:
+            # Log the action and import the classes from the Page Object files
+            self.logger.info("*** Adding Classes from Page Object files ***")
+            self.hp = HomePage(self.driver)
+            self.yc = YogaCollection(self.driver)
+            self.cl = CustLogin(self.driver)
+
             # Log the action and navigate to the main page
             self.logger.info("*** Navigate to the main page ***")
             self.driver.get(self.baseURL)
@@ -32,12 +38,10 @@ class TestAddToCompare:
 
             # Log the action and click the "Sign In" link
             self.logger.info("*** Click Sign In ***")
-            self.hp = HomePage(self.driver)
             self.hp.click_sign_in_lnk()
 
             # Log the action and enter the required credentials into the corresponding fields
             self.logger.info("*** Sign into the application ***")
-            self.cl = CustLogin(self.driver)
             self.cl.enter_email(self.user_email)
             self.cl.enter_pwd(self.password)
             self.cl.click_sign_in_btn()
@@ -51,23 +55,24 @@ class TestAddToCompare:
             self.yc = YogaCollection(self.driver)
             self.yc.click_fiona_wishlist()
 
-            # Log the action and check for the header title and that the item has been added to the Wish List
-            self.logger.info("*** Check to see if header and items displayed ***")
+            # Log the action and check that the user has been navigated to the right page and the item have been added
+            self.logger.info("*** Check to see if navigated to the right page and items displayed ***")
             self.wl = MyWishList(self.driver)
             self.wish_head = self.wl.wish_list_header()
             self.wish_list = self.wl.wish_list_fiona_short()
-            assert self.wish_head == "My Wish List" and self.wish_list
+            self.capt_url = self.wl.capture_url()
+            assert self.wish_head == "My Wish List" and self.wish_list and self.capt_url.startswith ("https://magento.softwaretestingboard.com/wishlist/index/index/wishlist_id")
 
             # Log the action and remove the items from the wish list
             self.logger.info("*** Removing items from wish list ***")
             self.wl.remove_fiona_shorts()
             self.driver.quit()
-            self.logger.info("*** Test_007_AddToWishList Passed ***")
+            self.logger.info("*** Test_005_AddToWishList Passed ***")
 
         except Exception as e:
             # Log the action and capture the screenshot of any failure
-            self.logger.info("*** Test_007_AddToWishList Failed ***")
+            self.logger.info("*** Test_005_AddToWishList Failed ***")
             screenshot_filename = os.path.join(screenshots_path, "test_add_to_wishlist.png")
             self.driver.save_screenshot(screenshot_filename)
             raise e
-        self.logger.info("*** Test_007_AddToWishList Complete ***")
+        self.logger.info("*** Test_005_AddToWishList Complete ***")
